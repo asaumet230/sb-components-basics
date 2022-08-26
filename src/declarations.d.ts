e113856606d16032b01cc7699d6f86eed07f0900
Andres Felipe Saumet <andressaumet@gmail.com>
2022-08-26 17:55:52 -0500

    feat: component compatibility

---
+++ src/declarations.d.ts
@@ -0,0 +1,9 @@
+/* 
+    Esta declaración 
+    permite que typescript 
+    transcriba los archivos module.css 
+    a la carpeta Dist, que se 
+    usan en determinado componente: 
+*/
+declare module '*.css';
+
